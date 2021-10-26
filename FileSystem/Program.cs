bee46e0e427ff5a8619fe56050eee4c682bd1bc0
Opilane TTHK <opilane@tthk.ee>
2021-10-26 14:04:15 +0300

    FileSystem

---
+++ FileSystem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace FileSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\opilane\samples";
+            string userDirectory = "wishlist";
+
+            string newDirectoryfullPath = @$"{rootDirectory}\{userDirectory}";
+            Console.WriteLine(newDirectoryfullPath);
+
+            Directory.CreateDirectory(newDirectoryfullPath);
+        }
+    }
+}
